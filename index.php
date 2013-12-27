73cf1f3c8bb7f3e862e2a3c10f22753075b0ecef
Villars <villars@HarryPotter.(none)>
2013-12-27 13:04:50 -0800

    initial commit from aws

---
+++ index.php
@@ -0,0 +1,5 @@
+<?require("/global/header.php");?>
+<link rel="stylesheet"  href='/assets/css/index.css' type='text/css'/>
+
+<p><img src="/assets/img/construction.jpeg" alt="under construction" /></p>
+<?require("/global/footer.php");?>
